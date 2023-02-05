d9422501c800d527a3d3da8b87f6a9ab57865674
Hamza <hamzaghnaim99@gmail.com>
2023-02-05 11:59:49 -0500

    #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import React,{Fragment} from "react";
 import "./App.css";
+// import { Outlet } from "react-router-dom";
 import Login from "./UI/Login";
 
 function App() {
-  return <h2>well done champ</h2>;
-}
+  return(
+    <Fragment> 
+      <Login/>
+    </Fragment> 
+)}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import Login from "./UI/Login";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <App /> }
+
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
