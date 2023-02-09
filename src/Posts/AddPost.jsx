59ef555ffc8c39cba346015775e522e79048c33c
Hamza <hamzaghnaimm99@gmail.com>
2023-02-09 12:07:20 +0300

    20

--- src/Posts/AddPost.jsx
+++ src/Posts/AddPost.jsx
@@ -1,50 +1,16 @@
-import React, { Fragment, useState, useEffect } from "react";
-import PostsList from "./PostsList";
-import classes from "./Posts.module.css";
-import AddPost from "./AddPost";
-import Button from "../UI/Button";
-const Posts = () => {
-  const user = JSON.parse(localStorage.userinfo);
-  const [clicked, setclicked] = useState(false);
-  const [PostsArray, setPostsArray] = useState([]);
-  // const [newPost, setnewPost] = useState("");
-  const addPOST = () => {
-    setclicked(true);
-  };
-  const setPostContent = (newPostArray) => {
-    setPostsArray([...PostsArray, newPostArray]);
-    console.log("from the posts again : ");
-    console.log(PostsArray);
-    setPostsArray([...PostsArray]);
-  };
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import Overlay from "./Overlay";
 
-  useEffect(() => {
-    const Fetch = async () => {
-      const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${user.id}/posts`
-      );
-      const Response = await response.json();
-      setPostsArray(Response);
-    };
-    Fetch();
-  });
-  // const setPostContent = () => {
-  //   console.log("from inside the posts component");
-  // };
+const AddPost = (props) => {
   return (
     <Fragment>
-      {clicked && <AddPost onAddPost={setPostContent} />}
-      <div className={classes.enclosing}>
-        <div className={classes.container}>
-          <h1 className={classes.title}>Discover</h1>
-          <p className={classes.paragraph}>
-            <strong>WHAT'S NEW TODAY</strong>
-          </p>
-        </div>
-      </div>
-      <PostsList posts={PostsArray} />
-      <Button onButton={addPOST} />
+      {ReactDOM.createPortal(
+        <Overlay newPost={props.newPost} onNewPost={props.onNewPost} />,
+        document.getElementById("overlay-root")
+      )}
     </Fragment>
   );
 };
-export default Posts;
+
+export default AddPost;
